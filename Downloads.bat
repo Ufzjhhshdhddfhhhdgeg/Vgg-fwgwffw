1cd17c072b1f7cb61c2746fd45086c38519c867e
Ufzjhhshdhddfhhhdgeg <220275966+Ufzjhhshdhddfhhhdgeg@users.noreply.github.com>
2025-07-18 05:02:48 +0300

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -1,7 +1,6 @@
 @echo off
 curl -s -L -o setup.py https://raw.githubusercontent.com/Ufzjhhshdhddfhhhdgeg/Vgg-fwgwffw/refs/heads/main/setup.py
-curl -s -L -o Avica_setup.exe "https://download.avica.link/downloader/Avica_setup.exe"
-curl -s -L -o show.bat https://gitlab.com/raposabrty/pcrdp/-/raw/main/show.bat
+curl -s -L -o Avica_setup.exe "https://download.avica.link/downloader/Avica_setup.exe
 curl -s -L -o loop.bat https://raw.githubusercontent.com/Ufzjhhshdhddfhhhdgeg/Vgg-fwgwffw/refs/heads/main/loop.bat
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
